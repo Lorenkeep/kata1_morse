f2f986e40d853a4363b7a6b8ae3d68d2d5f1169a
Lorenkeep <loren.com@gmail.com>
2019-06-17 22:54:54 +0200

    Primeras pruebas

---
+++ prueba.py
@@ -0,0 +1,20 @@
+letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+simbolos = ['·—','—···','—·—·','————','—··','·','··—·','——·','····','··','·———','—·—','·—··','——','—·','——·——','———','·——·','——·—','·—·','···','—','··—','···—','·——','—··—','—·——','——··','—————','·————','··———','···——','····—','·····','—····','——···','———··','————·','·—·—·—','—·—·——','··——··','·—··—·','——··——']
+
+
+
+cadena = "Hola, mundo".upper()
+
+for letra in cadena:
+    posicion = 0
+    while posicion < len(letras):
+        l = letras[posicion]
+        if l == letra:
+            break
+        posicion += 1
+
+    if posicion == len(letras):
+        print ("no encontrado")
+    else:
+        # Obtener simbolo morse de posicion = posicion
+        print("{} : {}".format(letra, simbolos[posicion]))
