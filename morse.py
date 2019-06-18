2c2c3b57fe0ca8a234f80972c8f0cad58508bf1d
Lorenkeep <loren.com@gmail.com>
2019-06-18 21:24:00 +0200

    diccionario a capon

--- morse.py
+++ morse.py
@@ -42,7 +42,49 @@ morse = {
     '"': '·—··—·',
     '!': '——··——'}
 
-reverso = {}
+reverso = {
+   '·—': 'A',
+   '—···': 'B',
+   '—·—·': 'C',
+   '—··': 'D',
+   '·': 'E',
+   '··—·': 'F',
+   '——·': 'G',
+   '····': 'H',
+   '··': 'I',
+   '·———': 'J',
+   '—·—': 'K',
+   '·—··': 'L',
+   '——': 'M',
+   '—·': 'N',
+   '——·——': 'Ñ',
+   '———': 'O',
+   '·——·': 'P',
+   '——·—': 'Q',
+   '·—·': 'R',
+   '···': 'S',
+   '—': 'T',
+   '··—': 'U',
+   '···—': 'V',
+   '·——': 'W',
+   '—··—': 'X',
+   '—·——': 'Y',
+   '——··': 'Z',
+   '—————': '0',
+   '·————': '1',
+   '··———': '2',
+   '···——': '3',
+   '····—': '4',
+   '·····': '5',
+   '—····': '6',
+   '——···': '7',
+   '———··': '8',
+   '————·': '9',
+   '·—·—·—': '.',
+   '—·—·——': ',',
+   '··——··': '?',
+   '·—··—·': '"',
+   '——··——': '!'}
 for key in morse:
     valor = morse[key]
     reverso[valor] = key
@@ -60,7 +102,14 @@ def toMorse(texto):
     return resultado
 
 def toPlain(codigo):
-   ## codigo = cadena.split(" ")
-    ##for letrita in 
-   ## if letrita in reverso:
-    pass
+    codigo = codigo.split(' ')
+    letras = ''
+    for caracter in codigo:
+        if caracter in reverso:
+            letras += reverso[caracter]
+
+        else:
+           letras += " "
+
+    return letras.capitalize()
+
--- prueba2.py
+++ prueba2.py
@@ -1,7 +1,8 @@
 import morse
 
 mensaje = input("Dime algo: ")
-codigomierda = morse.toPlain(codigo)
+
 telegrama = morse.toMorse(mensaje)
 print(telegrama)
-print(codigomierda)
+original = morse.toPlain(telegrama)
+print(original)
